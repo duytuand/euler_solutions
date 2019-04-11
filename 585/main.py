58be328e72adf2d1988a46af9239d79b5fe84a5b
Tuan Dao <16917615+duytuandao@users.noreply.github.com>
2019-04-11 13:24:23 +0100

    Create main.py

---
+++ 585/main.py
@@ -0,0 +1,32 @@
+from math import sqrt
+from lib.utils import halve, is_square
+
+def test()
+    """
+    test method to ensure core concepts 
+    are not compromised alongisde solution
+    development
+    """
+    pass
+
+def _F(n)
+    """
+    inner vision function
+    """
+    pass
+    
+def F(n)
+    """
+    orchestrator
+    """
+    pass
+
+def run():
+    """
+    entry point
+    """
+    pass
+
+if __name__ == '__main__':
+    test()
+    run()
