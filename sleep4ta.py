a0469d17cc5c259be2153310431f8753a152250c
Tuan Dao <16917615+duytuandao@users.noreply.github.com>
2019-08-01 12:03:16 +0100

    Update sleep4ta.py

--- sleep4ta.py
+++ sleep4ta.py
@@ -1,20 +1,28 @@
-# sleep4ta
-
 import argparse
 
-success_rate = 0.5
+success_rate = 1.00/6
 num_throws = 1.00/success_rate
 
 def run(n: int) -> int:
-	if n == 1:
-		return 2
-	k = run(n-1) 	# expected number of throws for n-1
-	k = k + 1 		# because we throw 1 add'l time as test for n	
-	return k*num_throws
-	
+    # base case
+    if n == 0:
+        return 0
+        
+    # expected number of throws for n-1
+    k = run(n-1)    
+    
+    # throw 1 add'l time as test for n
+    k += 1
+
+    # try num_throws times ( expected # of throws)
+    return k*num_throws
+    
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument("n", type=int)
-	args = parser.parse_args()
-	n = args.n
-	print(run(n))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("n", type=int)
+    args = parser.parse_args()
+    n = args.n
+    try:
+        print(run(n))
+    except:
+        print("Cannot be solved. TA can't sleep")
