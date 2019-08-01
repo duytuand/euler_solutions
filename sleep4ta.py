2b394d0da898c8dbc9a2b5510a19d6cf0dc2edc4
Tuan Dao <16917615+duytuandao@users.noreply.github.com>
2019-08-01 12:06:38 +0100

    Update sleep4ta.py

--- sleep4ta.py
+++ sleep4ta.py
@@ -1,21 +1,18 @@
+#sleep4ta.py
+
 import argparse
 
-success_rate = 1.00/6
+# use 6 instead of 2 for dice rolls
+success_rate = 1.00/2
 num_throws = 1.00/success_rate
 
 def run(n: int) -> int:
     # base case
     if n == 0:
         return 0
-        
-    # expected number of throws for n-1
-    k = run(n-1)    
-    
-    # throw 1 add'l time as test for n
-    k += 1
-
-    # try num_throws times ( expected # of throws)
-    return k*num_throws
+    k = run(n-1)         # expected number of throws for n-1
+    k += 1               # throw 1 add'l time as test for n
+    return k*num_throws  # try num_throws times ( expected # of throws)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
